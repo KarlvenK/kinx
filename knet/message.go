4b45fe6217034851d380e480b25099a0407bd65f
Karlven <karlven@outlook.com>
2022-01-12 23:01:11 +0800

    message module

---
+++ knet/message.go
@@ -0,0 +1,39 @@
+package knet
+
+type Message struct {
+	Id      uint32
+	DataLen uint32
+	Data    []byte
+}
+
+func NewMsgPackage(id uint32, data []byte) *Message {
+	return &Message{
+		DataLen: uint32(len(data)),
+		Id:      id,
+		Data:    data,
+	}
+}
+
+func (m *Message) GetMsgId() uint32 {
+	return m.Id
+}
+
+func (m *Message) GetMsgLen() uint32 {
+	return m.DataLen
+}
+
+func (m *Message) GetData() []byte {
+	return m.Data
+}
+
+func (m *Message) SetMsgId(id uint32) {
+	m.Id = id
+}
+
+func (m *Message) SetDataLen(length uint32) {
+	m.DataLen = length
+}
+
+func (m *Message) SetData(data []byte) {
+	m.Data = data
+}
